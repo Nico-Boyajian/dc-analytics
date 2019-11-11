98d94ae250c0858486883a4f9956f8ed5b8d4832
Aditya Yellajosyula <ayellajosyula@gmail.com>
2019-11-11 18:21:53 -0500

    Pushing final version of previous project files

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiamFjb2JrbHVnIiwiYSI6ImNrMTZ6bGd5YTFhaW8zYnBrdHJ4dDh0aWQifQ.1gslF2GPLLud6HTe0ZK56w";
+const API_KEY = "pk.eyJ1IjoiYXN5ZWxsYWpvc3l1bGEiLCJhIjoiY2sxNnpoYzJwMWI3MzNrbzNhNzRwdmxlZiJ9.nnyhyBQyolcOtjIFQEoIXg";
